5562a73c293925283b84c69c32aa74338357026c
Yuvraj Shandilya <yuvrajshandilya05@gmail.com>
2024-06-22 21:35:32 +0530

    add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { 
+    changeCurrentPassword,
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage 
+} from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js';
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 
 
 const router = Router();
@@ -23,6 +36,19 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //secured routes
+//verify jwt to know whether use is login or not
 router.route("/logout").post(verifyJWT , logoutUser )
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT , changeCurrentPassword)
+router.route("/current-user").get(verifyJWT , getCurrentUser)
+//.patch se selected hi update hoga warna sb ho jayega post se 
+router.route("/update-account").patch(verifyJWT , updateAccountDetails)
+router.route("/avatar").patch(verifyJWT , upload.single("avatar"),updateUserAvatar)
+//avatar.single("avatar")ka matlab hai single file hi upload hogi and uska naam avatar hoga
+router.route("/cover-image").patch(verifyJWT , upload.single("/coverImage"),updateUserCoverImage)
+
+//jab params se lena hai data to iss form mein lo and : ke baad wahi likhna jisme expect kr rehe ho
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 export default router;
